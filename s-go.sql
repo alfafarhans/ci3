ec7c8cd2137a8c6ceb7191d1c4f9a7a1f8b133de
Nzatf <43507001+Nzatf@users.noreply.github.com>
2019-09-24 00:11:08 +0700

    Update s-go.sql

--- s-go.sql
+++ s-go.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2019 at 07:12 PM
+-- Generation Time: Sep 23, 2019 at 07:02 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -25,13 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `seminar`
+--
+
+CREATE TABLE `seminar` (
+  `seminar_id` int(11) NOT NULL,
+  `seminar_name` varchar(50) NOT NULL,
+  `seminar_date` datetime NOT NULL,
+  `seminar_held` varchar(50) NOT NULL,
+  `seminar_desc` text NOT NULL,
+  `seminar_price` int(11) NOT NULL,
+  `seminar_drcode` varchar(25) NOT NULL,
+  `seminar_banner` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `email` varchar(25) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `email` varchar(35) NOT NULL,
   `first_name` varchar(15) NOT NULL,
   `last_name` varchar(10) NOT NULL,
   `user_phone` int(15) NOT NULL,
@@ -43,14 +60,31 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `password`, `email`, `first_name`, `last_name`, `user_phone`, `user_address`) VALUES
-(5, '202cb962ac59075b964b', 'irfannz@outlook.com', 'asd', 'asd', 0, ''),
-(6, 'd41d8cd98f00b204e980', '', '', '', 0, ''),
-(7, '202cb962ac59075b964b', '123', '123', '123', 0, ''),
-(8, 'bcbe3365e6ac95ea2c03', 'irfannz@outlook.com1', '231', '23123', 0, '');
+(33, 'e77989ed21758e78331b20e477fc5582', 'mynameisnazmi41@gmail.com', 'Muhammad', 'Nazmi', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_trx`
+--
+
+CREATE TABLE `user_trx` (
+  `booking_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `seminar_id` int(11) NOT NULL,
+  `payment_id` int(11) NOT NULL,
+  `atten_status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `seminar`
+--
+ALTER TABLE `seminar`
+  ADD PRIMARY KEY (`seminar_id`);
 
 --
 -- Indexes for table `user`
@@ -59,14 +93,28 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
 --
+-- Indexes for table `user_trx`
+--
+ALTER TABLE `user_trx`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `user_idfk` (`user_id`) USING BTREE,
+  ADD KEY `seminar_idfk` (`seminar_id`) USING BTREE;
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `seminar`
+--
+ALTER TABLE `seminar`
+  MODIFY `seminar_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
