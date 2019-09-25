1e6e008192a02de3fbf92f0da9d28b630384ff31
alfafarhans <55489578+alfafarhans@users.noreply.github.com>
2019-09-25 21:17:06 +0700

    Add files via upload

---
+++ application/views/event_detail.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>asset/css/event_detail.css">
+	
+</head>
+<body>
+    <div id="wrapper">
+    <!-- bagian navbar  -->
+    <div id="top">
+        <div id="navbar_kiri">
+            <a href="<?php echo base_url();?>index.php/"> Seminar Go </a>
+        </div>
+        <div id="navbar_kanan">
+            <a href="<?php echo base_url();?>index.php/ads">Advertising </a> 
+            <a href="<?php echo base_url();?>index.php/login">Sign in </a>
+        </div>
+    </div>   
+
+    <!-- bagian isi  -->
+    <div id="body">
+        <div id="bodyartikel2">
+            <div id="flexbod">
+                <div id="leftposttop">
+                    <img src="../asset/pict/seminar1.png">
+                </div>
+        
+                <div id="rightposttop">
+                    <div id="padding">
+                        <h2> Indonesia Ves 2019 </h2> 
+                        <p> Rp. 35.000 </p>
+                    </div>
+                </div>
+            </div>
+
+            <div id="flexbod">
+                <div id="leftpost">
+                    <div id="padding">
+                        <a href="#"> Daftar </a>
+                    </div>
+                </div>
+        
+                <div id="rightpost">
+                    <div id="padding">
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div id="flexbod">
+                <div id="leftpost">
+                    <div id="obj-post">
+                        <div id="obj-judul"> Date and Time </div>
+                        <p> Wed, 10 April 2019, 12.30
+                    </div>
+
+                    <div id="obj-post">
+                        <div id="obj-judul"> Location </div>
+                        <p> Senayan, Jakarta Selatan
+                    </div>
+                </div>
+        
+                <div id="rightpost">
+                    <div id="obj-post">
+                        <div id="obj-judul"> Description </div>
+                        <p> Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+                    </div>
+
+                    <div id="obj-post">
+                        <div id="obj-judul"> Tag </div>
+                        <p> <a href="#">bussiness</a> <a href="#">kewirausahaan</a> <a href="#">enterpreneur</a> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <!-- bagian footer  -->
+
+    <div id="infofooter600">
+    </div>
+
+    <div id="footer">
+        <p>Copyright © 2019 </p>
+    </div>
+    </div>
+</body>
+</html>
---
+++ application/views/home.php
@@ -0,0 +1,195 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>asset/css/home.css">
+	
+</head>
+<body>
+    <div id="wrapper">
+    <!-- bagian navbar  -->
+    <?php if(isset($user_id))
+    {
+        echo '
+        <div id="top">
+            <div id="navbar_kiri">
+                <a href="#"> Seminar Go </a>
+            </div>
+            <div id="navbar_kanan">
+                <a href="'.base_url().'index.php/logout">LOGOUT </a> 
+            </div>
+        </div> ';
+    }
+    else{
+        echo '
+        <div id="top">
+            <div id="navbar_kiri">
+                <a href="'.base_url().'index.php/"> Seminar Go </a>
+            </div>
+            <div id="navbar_kanan">
+                
+                <a href="'.base_url().'index.php/ads">Advertising </a> 
+                <a href="'.base_url().'index.php/login">Sign in</a>
+                
+                <div id="dropdown">
+                    <img src="../asset/pict/profile1.png">
+
+                    <div id="dropdown-content">
+                        <a href="#"> Profile </a> <br> <br>
+                        <a href="#"> My Event </a> <br> <br>
+                        <a href="#"> Sign Out </a>
+                    </div>
+                </div>
+
+            </div>
+        </div> ';
+    }
+    
+    ?>  
+
+    <!-- bagian header  -->
+    <div id="header">
+        <img src="<?php echo base_url();?>asset/pict/header1.png">
+    </div>
+
+    <!-- bagian pencarian  -->
+    <div id="smart">
+        <div id="item">
+            <label for="seminar"> Seminar </label>
+            <input type="text" id="seminar" name="seminar">
+        </div>
+
+        <div id="item">
+            <label for="location"> Location </label>
+            <input type="text" id="location" name="location" >
+        </div>
+
+        <div id="item">
+            <label for="date"> Date </label>
+            <br>
+            <select name="date">
+            <option value="anydate" selected> Any Date </option>
+            <option value="today"> Today </option>
+            <option value="tomorrow"> Tomorrow</option>
+            <option value="thisweekend"> This Weekend</option>
+            <option value="thisweek"> This Week</option>
+            <option value="nextweek"> Next Week</option>
+            <option value="thismonth"> This Month</option>
+            <option value="nextmonth"> Next Month</option>
+            <select>
+        </div>
+        <div id="item">
+            <label for="category"> Category </label>
+            <br>
+            <select name="category" >
+            <option value="anycategory" selected> Any Category </option>
+            <option value="otomotif"> Auto, Boat & air</option>
+            <option value="business"> Business</option>
+            <option value="charity"> Charity & Causes</option>
+            <option value="family"> Family & Education</option>
+            <option value="fashion"> Fashion</option>
+            <option value="film"> Film & Media</option>
+            <option value="fooddrink"> Food & Drink</option>
+            <option value="goverment"> Govevrment </option>
+            <option value="health"> Health</option>
+            <option value="hobbies"> Hobbies</option>
+            <option value="holiday"> Holiday</option>
+            <option value="homelifefstyle"> Home & Lifefstyle</option>
+            <option value="schoolactivies"> School Activies</option>
+            <option value="sciencetech"> Science & Tech</option>
+            <option value="spiritually"> Spiritually</option>
+            <option value="sportitness"> Sport & Fitness</option>
+            <option value="traveloutdoor"> Travel & Outdoor</option>
+            <select>
+        </div>
+
+        <div id="item">
+            <label for="category"> Price </label>
+            <br>
+            <select name="category" >
+            <option value="anyprice" selected> Any Price </option>
+            <option value="free"> Free </option>
+            <option value="paid"> Paid </option>
+            <select>
+        </div>
+    </div>   
+    
+    <!-- bagian isi  -->
+    <div id="body">
+    <div id="postinduk">
+            <div id="post">
+                <a href="<?php echo base_url();?>index.php/event_detail"> <img src="<?php echo base_url();?>asset/pict/seminar1.png"> </a>
+                <div id="descbox"> 
+                    <div id="namaseminar"> Indonesia Ves 2019 </div>
+                    <div id="dateseminar"> Wed, 10 April, 12.30</div>
+                    <div id="locseminar"> Senayan, Jakarta Selatan</div>
+                </div>
+            </div>
+
+            <div id="post">
+                <img src="<?php echo base_url();?>asset/pict/seminar2.png">
+                <div id="descbox"> 
+                    <div id="namaseminar"> National Youth Summit </div>
+                    <div id="dateseminar"> Sun, 14 April, 12.00 </div>
+                    <div id="dateseminar"> Hotel Grand Sahid, Jakarta </div>
+                </div>
+            </div>
+        </div>
+        
+        <div id="postinduk"> 
+            <div id="post">
+                <img src="<?php echo base_url();?>asset/pict/seminar3.png">
+                <div id="descbox"> 
+                    <div id="namaseminar"> Machine Learning dan IoT </div>
+                    <div id="dateseminar"> Sat, 31 Agustus, 08.00 </div>
+                    <div id="dateseminar"> Auditorium Lt7 Mercubuana Meruya, Jakarta </div>
+                </div>
+            </div>
+          
+            <div id="post">
+                <img src="<?php echo base_url();?>asset/pict/seminar1.png">
+                <div id="descbox"> 
+                    <div id="namaseminar"> Indonesia Ves 2019 </div>
+                    <div id="dateseminar"> Wed, 10 April, 12.30</div>
+                    <div id="locseminar"> Senayan, Jakarta Selatan</div>
+                </div>
+            </div>
+        </div>
+        
+        <div id="postinduk">  
+
+            <div id="post">
+                <img src="<?php echo base_url();?>asset/pict/seminar2.png">
+                <div id="descbox"> 
+                    <div id="namaseminar"> National Youth Summit </div>
+                    <div id="dateseminar"> Sun, 14 April, 12.00 </div>
+                    <div id="dateseminar"> Hotel Grand Sahid, Jakarta </div>
+                </div>
+            </div>
+
+            <div id="post">
+                <img src="<?php echo base_url();?>asset/pict/seminar3.png">
+                <div id="descbox"> 
+                    <div id="namaseminar"> Machine Learning dan IoT </div>
+                    <div id="dateseminar"> Sat, 31 Agustus, 08.00 </div>
+                    <div id="dateseminar"> Auditorium Lt7 Mercubuana Meruya, Jakarta </div>
+                </div>
+            </div>
+        
+        </div> 
+    </div>
+
+    <!-- bagian footer  -->
+
+    <div id="infofooter">
+    </div>
+    
+    <div id="footer">
+        <p>Copyright © 2019 </p>
+    </div>
+    </div>
+</body>
+</html>
