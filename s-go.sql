1ce64b57786b7231c0c8118eac9e0c09c99c9644
Nzatf <43507001+Nzatf@users.noreply.github.com>
2019-09-29 11:56:04 +0700

    Update s-go.sql

--- s-go.sql
+++ s-go.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 23, 2019 at 07:02 PM
+-- Generation Time: Sep 29, 2019 at 06:55 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -34,10 +34,23 @@ CREATE TABLE `seminar` (
   `seminar_date` datetime NOT NULL,
   `seminar_held` varchar(50) NOT NULL,
   `seminar_desc` text NOT NULL,
-  `seminar_price` int(11) NOT NULL,
+  `seminar_tag` text NOT NULL,
+  `seminar_price` decimal(15,3) NOT NULL,
   `seminar_drcode` varchar(25) NOT NULL,
-  `seminar_banner` varchar(25) NOT NULL
+  `seminar_banner` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `seminar`
+--
+
+INSERT INTO `seminar` (`seminar_id`, `seminar_name`, `seminar_date`, `seminar_held`, `seminar_desc`, `seminar_tag`, `seminar_price`, `seminar_drcode`, `seminar_banner`) VALUES
+(1, 'Indonesia Ves 2019', '2019-10-16 08:55:00', 'Konoha, Konohagakure', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Vitae et leo duis ut diam quam. Erat imperdiet sed euismod nisi porta. Adipiscing at in tellus integer feugiat scelerisque. Porttitor rhoncus dolor purus non. ', 'Saber,Shirou,SASUKEEE!!!!', '6.000', 'Batik SASUKEEE!!', 'Indonesia_Ves_2019_2019_2019-09_12.png'),
+(2, 'National TEST', '2019-09-26 05:14:19', 'Senayan, Otogakure', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra. Consectetur libero id faucibus nisl tincidunt eget. Pretium viverra suspendisse potenti nullam ac tortor vitae. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. At risus viverra adipiscing at in tellus. Eu nisl nunc mi ipsum faucibus vitae. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Sed cras ornare arcu dui vivamus arcu felis bibendum ut. Et molestie ac feugiat sed lectus vestibulum mattis. Morbi tincidunt ornare massa eget', 'Saber,Shirou,SASUKEEE!!!!', '5.000', 'Batik Kuning', 'National_Youth_Summit_2019_09_26.png'),
+(3, 'National Youth Summit', '2019-09-27 05:14:19', 'Senayan, Sunagakure', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra. Consectetur libero id faucibus nisl tincidunt eget. Pretium viverra suspendisse potenti nullam ac tortor vitae. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. At risus viverra adipiscing at in tellus. Eu nisl nunc mi ipsum faucibus vitae. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Sed cras ornare arcu dui vivamus arcu felis bibendum ut. Et molestie ac feugiat sed lectus vestibulum mattis. Morbi tincidunt ornare massa eget', 'Saber,Shirou,SASUKEEE!!!!', '150.500', 'Batik oren', 'National_Youth_Summit_2019_09_27.png'),
+(4, 'Machine Learning dan IoT', '2019-09-30 05:14:19', 'Senayan, Takumi no Sato', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra. Consectetur libero id faucibus nisl tincidunt eget. Pretium viverra suspendisse potenti nullam ac tortor vitae. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. At risus viverra adipiscing at in tellus. Eu nisl nunc mi ipsum faucibus vitae. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. Sed cras ornare arcu dui vivamus arcu felis bibendum ut. Et molestie ac feugiat sed lectus vestibulum mattis. Morbi tincidunt ornare massa eget', 'Saber,Shirou,SASUKEEE!!!!', '35.000', 'Batik Kuning', 'Machine_Learning_dan_IoT_2019_09_30.png'),
+(5, 'Indonesia E-FEST 2060', '2020-03-19 09:42:00', 'Senayan,  Kirigakure', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Vitae et leo duis ut diam quam. Erat imperdiet sed euismod nisi porta. Adipiscing at in tellus integer feugiat scelerisque. Porttitor rhoncus dolor purus non. ', 'Saber,Shirou,SASUKEEE!!!!', '60.000', 'Batik Hitam', 'Indonesia_Ves_2019_2019_2019-09_12.png'),
+(6, 'Indonesia Ves 2019', '2019-09-12 08:00:00', 'Senayan, Kumogakure', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Vitae et leo duis ut diam quam. Erat imperdiet sed euismod nisi porta. Adipiscing at in tellus integer feugiat scelerisque. Porttitor rhoncus dolor purus non. ', 'Saber,Shirou,SASUKEEE!!!!', '500.000', 'Batik Hitam', 'Indonesia_Ves_2019_2019_2019-09_12.png');
 
 -- --------------------------------------------------------
 
@@ -60,7 +73,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `password`, `email`, `first_name`, `last_name`, `user_phone`, `user_address`) VALUES
-(33, 'e77989ed21758e78331b20e477fc5582', 'mynameisnazmi41@gmail.com', 'Muhammad', 'Nazmi', 0, '');
+(33, 'e77989ed21758e78331b20e477fc5582', 'mynameisnazmi41@gmail.com', 'Muhammad', 'Nazmi', 0, ''),
+(35, 'e77989ed21758e78331b20e477fc5582', 'fajarbarokah98@yahoo.co.id', 'fajar', 'barokah', 0, ''),
+(36, '7815696ecbf1c96e6894b779456d330e', 'irfaasdnnz@outlook.com', 'asd', 'asd', 0, '');
 
 -- --------------------------------------------------------
 
@@ -108,13 +123,13 @@ ALTER TABLE `user_trx`
 -- AUTO_INCREMENT for table `seminar`
 --
 ALTER TABLE `seminar`
-  MODIFY `seminar_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `seminar_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
