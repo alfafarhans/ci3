10ca7212a2b14d2b021396c644539961ddc58106
Nzatf <43507001+Nzatf@users.noreply.github.com>
2019-10-24 18:57:14 +0700

    ngubah CSS dikit

--- application/views/event_detail.php
+++ application/views/event_detail.php
@@ -25,19 +25,7 @@
 				});
         }
 
-    /* Icon User Click Close
-    window.onclick = function(event) {
-        if (!event.target.matches('.dropbtn')) {
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    }*/
+
     window.onclick = function(event) {
         if ( (!event.target.matches('.dropdown')) &&(!event.target.matches('.imgdrop')) && (!event.target.matches('.result_sem')) && (!event.target.matches('.result_loc')) && (!event.target.matches('.p')) ){
             $('#jcdrop').slideUp("fast");
@@ -50,6 +38,56 @@
    
 
     $(function() {
+        //avg color
+var rgb = getAverageRGB(document.getElementById('imps'));
+document.getElementById('rightposttop').style.backgroundColor = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')';
+document.getElementById('leftposttop').style.backgroundColor = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')';
+ 
+function getAverageRGB(imgEl) {
+    
+    var blockSize = 5, // only visit every 5 pixels
+        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data, width, height,
+        i = -4,
+        length,
+        rgb = {r:0,g:0,b:0},
+        count = 0;
+        
+    if (!context) {
+        return defaultRGB;
+    }
+    
+    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+    
+    context.drawImage(imgEl, 0, 0);
+    
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+        /* security error, img on diff domain */alert('x');
+        return defaultRGB;
+    }
+    
+    length = data.data.length;
+    
+    while ( (i += blockSize * 4) < length ) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i+1];
+        rgb.b += data.data[i+2];
+    }
+    
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r/count);
+    rgb.g = ~~(rgb.g/count);
+    rgb.b = ~~(rgb.b/count);
+    
+    return rgb;
+    
+}
         let a1 = $('#a1').html();
         if(a1 == "Booked"){
             $('#a1').attr("href","javascript:void(0);");
@@ -161,7 +199,7 @@
                         }
                         echo'
                     <div id="leftposttop">
-                        <img src="'.base_url().'asset/pict/banner/'. $value['seminar_banner'].'">
+                        <img id="imps" src="'.base_url().'asset/pict/banner/'. $value['seminar_banner'].'">
                     </div>
             
                     <div id="rightposttop">
