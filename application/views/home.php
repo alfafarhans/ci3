948af56e0c380bfdef583b3e09864f394b8ca34c
Nzatf <43507001+Nzatf@users.noreply.github.com>
2019-11-15 21:12:36 +0700

    a

--- application/views/home.php
+++ application/views/home.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     }
      //for page
     $(function() {
-        
+ 
         let arrcattext = [
                     "Otomotif",
                     "Business",
