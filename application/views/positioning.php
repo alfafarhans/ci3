63019a88824c1693daae40667c00b9ef8cc7e80c
Nzatf <43507001+Nzatf@users.noreply.github.com>
2019-11-24 14:03:49 +0700

    a

--- application/views/positioning.php
+++ application/views/positioning.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <script type="text/javascript" src="<?php echo base_url();?>asset/js/jquery-3.4.1.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>asset/js/jquery-ui.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>asset/js/jquery-ui.min.js"></script>
 <style>
 	body{
 		margin:0;/*dont*/
