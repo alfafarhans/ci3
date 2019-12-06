3099f67b18aa6fd8a430bf08b472b5902b783e98
alfafarhans <55489578+alfafarhans@users.noreply.github.com>
2019-12-07 01:07:56 +0700

    verified page

---
+++ application/views/verified.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/asset/css/verified.css">
+	
+</head>
+<body>
+    <div id="wrapper">
+
+    <div id="topr">
+    </div>
+
+    <!-- bagian navbar  -->
+    <div id="top">
+        <div id="navbar_kiri">
+            <a href="<?php echo base_url();?>home"> Seminar Go </a>
+        </div>
+    </div>   
+
+    <!-- bagian isi  -->
+
+    <div id="body">
+        <div id="bodyartikel2">
+            <div id="toppost">
+                <div id="objtop">
+                    <center>
+                    <h1> Attendance Information </h1> 
+                    Your presence status is seen in the following information below
+                    </center>
+                </div>
+            </div>
+
+            <div id="mainright">
+                <div id="objright2">
+                    <center>
+                    Thank you to <b> Alfa Farhan Syarief </b> for attending in 
+                    <h2>Machine Learning dan IoT</h2>
+                    on <b id="red"> Saturday, 07 December 2019 </b>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- bagian footer  -->
+
+    <div id="infofooter600">
+    </div>
+
+    <div id="footer">
+        <p>Copyright © 2019 </p>
+    </div>
+    </div>
+</body>
+</html>
