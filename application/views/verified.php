8294338277ad497d9bf7a9bcca5069f6440a1639
alfafarhans <55489578+alfafarhans@users.noreply.github.com>
2019-12-11 23:34:01 +0700

    verified mobile friendly

--- application/views/verified.php
+++ application/views/verified.php
@@ -7,6 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/asset/css/verified.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/asset/css/verified_resolusi.css">
 	
 </head>
 <body>
